aea88a0d9fb48b26e0650d5276524faa88e18dcf
liyanpeng <liyanpengwork@gmail.com>
2020-06-07 00:31:01 +0800

    build: add spring dependencies
    # add spring dependencies

--- shiro/src/main/java/com/spring/shiro/ShiroApplication.java
+++ shiro/src/main/java/com/spring/shiro/ShiroApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
-@MapperScan
+@MapperScan("com.spring.shiro.dao")
 public class ShiroApplication {
 
 	public static void main(String[] args)  {
