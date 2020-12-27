23edb42760869c955186695843b32e523af4aa8a
liyanpeng <liyanpengwork@gmail.com>
2020-12-27 23:50:55 +0800

    author: liyanpeng learning leetcode two sum

--- design-patterns/src/test/java/com/spring/design/patterns/DesignPatternsApplicationTests.java
+++ design-patterns/src/test/java/com/spring/design/patterns/DesignPatternsApplicationTests.java
@@ -8,6 +8,33 @@ class DesignPatternsApplicationTests {
 
     @Test
     void contextLoads() {
+        Bug bug = new Bug("a", "desc");
     }
 
+}
+
+class Bug {
+    private String name;
+    private String desc;
+
+    public Bug(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
 }
